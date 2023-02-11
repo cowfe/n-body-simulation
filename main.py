561e78890b80023164c0243ca418484cd8bc556a
Zack <cowfe@hotmail.com>
2023-02-11 00:33:21 -0800

    Update color

--- main.py
+++ main.py
@@ -122,12 +122,12 @@ def solar():
     planets = [
         Planet(type='EARTH', mass=0.000001, position=[0, 15, 0.0], velocity=[-0.5, 0, 0.0], image=image_jupiter, radius=2, trace_color=(0, 100, 150)),
         Planet(type='EARTH', mass=0.000001, position=[0, 30, 0.0], velocity=[-0.3, 0.0, 0.0], image=image_venus, radius=5, trace_color=(117, 117, 117)),
-        Planet(type='EARTH', mass=0.000001, position=[0, 45, 0.0], velocity=[-0.2, 0.0, 0.0], image=image_earth, radius=5, trace_color=(0, 80, 190)),
+        Planet(type='EARTH', mass=0.000001, position=[0, 45, 0.0], velocity=[-0.2, 0.0, 0.0], image=image_earth, radius=5, trace_color=(10, 80, 190)),
         Planet(type='EARTH', mass=0.000001, position=[0, 60, 0.0], velocity=[-0.15, 0.0, 0.0], image=image_mars, radius=5, trace_color=(70, 100, 210)),
-        Planet(type='EARTH', mass=0.000001, position=[5, 80, 0.0], velocity=[-0.2, 0.0, 0.0], image=image_jupiter, radius=8, trace_color=(0, 100, 210)),
+        Planet(type='EARTH', mass=0.000001, position=[5, 80, 0.0], velocity=[-0.2, 0.0, 0.0], image=image_jupiter, radius=8, trace_color=(60, 132, 171)),
         Planet(type='EARTH', mass=0.000001, position=[10, 95, 0.0], velocity=[-0.21, 0.0, 0.0], image=image_saturn, radius=12, trace_color=(50, 100, 210)),
         Planet(type='EARTH', mass=0.000001, position=[20, 100, 0.0], velocity=[-0.25, 0.0, 0.0], image=image_jupiter, radius=6, trace_color=(20, 120, 210)),
-        Planet(type='EARTH', mass=0.000001, position=[-10, 105, 0.0], velocity=[-0.15, 0.0, 0.0], image=image_uranus, radius=6, trace_color=(10, 100, 110)),
+        Planet(type='EARTH', mass=0.000001, position=[-10, 105, 0.0], velocity=[-0.15, 0.0, 0.0], image=image_uranus, radius=6, trace_color=(96, 150, 180)),
         Planet(type='EARTH', mass=0.000001, position=[0, 110, 0.0], velocity=[-0.25, 0.0, 0.0], image=image_jupiter, radius=2, trace_color=(50, 130, 190)),
         Planet(type='SUN', mass=5.0, position=[0, 0.0, 0.0], velocity=[0.0, 0.0, 0.0], image=image_sun, radius=20, trace_color=(206, 66, 87)),
     ]
